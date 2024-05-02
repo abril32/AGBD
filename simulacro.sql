d7657da81748191a91790a271f8a3acca84a92f7
Abril <lagartosautos@gmail.com>
2024-05-02 11:50:30 -0300

    Ejercicio 5

--- simulacro.sql
+++ simulacro.sql
@@ -13,6 +13,10 @@ SELECT country,city FROM city ci
 JOIN country c on ci.country_id = c.country_id
 ORDER by c.country ASC
 /*5*/
+SELECT country,count(ci.city) as cant_ciudad FROM city ci
+JOIN country c on ci.country_id = c.country_id
+GROUP by c.country_id
+ORDER by cant_ciudad DESC
 /*6*/
 /*7*/
 /*8*/
