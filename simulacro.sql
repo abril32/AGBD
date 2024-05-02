0875af25841a327a16272e69457ce224baedcbcc
Abril <lagartosautos@gmail.com>
2024-05-02 11:30:53 -0300

    Ejercicio 3

--- simulacro.sql
+++ simulacro.sql
@@ -6,6 +6,7 @@ SELECT DISTINCT rating FROM film
 ORDER by rating ASC
 /*3*/
 SELECT DISTINCT title,replacement_cost,rating FROM film
+WHERE rating like '%PG%'
 ORDER by replacement_cost DESC,title ASC
 /*4*/
 SELECT country,city FROM city ci
