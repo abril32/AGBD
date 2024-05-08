dae2cf8344d467696cdf9e76ebdcfe54afdfe038
Abril Mosquera <lagartosautos@gmail.com>
2024-05-08 11:03:15 -0300

    Simulacro hr ejercicio 2

--- simulacro_hr.sql
+++ simulacro_hr.sql
@@ -1,13 +1,10 @@
 /*1*/
 SELECT department_name FROM departments
-ORDER by department_name ASC
+ORDER by department_id ASC
 /*2*/
 SELECT DISTINCT salary FROM employees
 ORDER by salary DESC
 /*3*/
-SELECT job_title, min_salary, max_salary FROM jobs
-WHERE job_title like '%Manager'
-ORDER by max_salary DESC
 /*4*/
 /*5*/
 /*6*/
