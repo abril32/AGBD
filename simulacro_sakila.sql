3160c8da181efbb11cc1671e6dbc700b83548a4b
Abril Mosquera <lagartosautos@gmail.com>
2024-05-16 09:51:56 -0300

    simulacro sakila terminado del 7 al 10

--- simulacro_sakila.sql
+++ simulacro_sakila.sql
@@ -24,8 +24,23 @@ GROUP by c.country_id
 HAVING count(ci.city) >= 2
 ORDER by count(ci.city) ASC
 /*7 falta*/
-SELECT rental_date FROM rental
-WHERE rental_date
+SELECT min(rental_date), max(rental_date) FROM rental
 /*8*/
+SELECT count(fa.actor_id) as cant_Actors, f.title FROM film f
+JOIN film_actor fa on f.film_id = fa.film_id
+GROUP BY fa.actor_id
+ORDER BY cant_Actors ASC
+LIMIT 10
 /*9*/
+SELECT count(rental_date) as fecha_renta,title FROM film f
+JOIN inventory i on f.film_id = i.film_id 
+JOIN rental r on i.inventory_id = r.inventory_id
+GROUP by i.film_id
+ORDER BY fecha_renta DESC
+LIMIT 1 OFFSET 2
 /*10*/
+SELECT round(avg(actores),2), max(actores) FROM (
+select f.film_id,title, count(actor_id) as actores FROM film_actor fa
+JOIN film f ON f.film_id = fa.film_id
+WHERE length BETWEEN 90 AND 120
+GROUP BY f.film_id)
