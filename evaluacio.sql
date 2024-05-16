3f255ce7334fba357285d96e353c1e22739df962
Abril Mosquera <lagartosautos@gmail.com>
2024-05-16 11:05:05 -0300

    Evaluacion, ejercicio 4

--- evaluacio.sql
+++ evaluacio.sql
@@ -23,6 +23,10 @@ ORDER by name asc
 -- de la persona que vive en la tercera casa
 -- de la calle 'Franklin Ave' (ordenadas por la altura
 -- de la calle)
+SELECT name,address_number,address_street_name FROM person
+WHERE address_street_name = 'Franklin Ave'
+ORDER by address_number ASC
+LIMIT 1 OFFSET 2
 
 -- 5) Encontrar la edad mínima, máxima y promedio
 -- de los varones que tienen licencia de conducir
